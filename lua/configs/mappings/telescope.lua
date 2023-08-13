fecc69832ba1b6e25ac95c9f11297f8ec3bd5555
GreatestTrain <96893357+GreatestTrain@users.noreply.github.com>
2023-08-13 15:51:03 -0500

    Sun Aug 13 15:51:03 -05 2023

--- lua/configs/mappings/telescope.lua
+++ lua/configs/mappings/telescope.lua
@@ -6,8 +6,15 @@ M.mappings = {
 		f = { ':Telescope find_files<CR>', 'find files' },
 		g = { ':Telescope grep_string<CR>', 'find the current word' },
 		r = { ':Telescope live_grep<CR>', 'find in workspace' },
+	},
+	['<leader>g'] = {
+		name = 'git',
+		b = { ':Telescope git_branches<CR>', 'select git branch' },
+		f = { ':Telescope git_files<CR>', 'git files' },
+		s = { ':Telescope git_status<CR>', 'git status' },
+		t = { ':Telescope git_stash<CR>', 'git stash' },
+		c = { ':Telescope git_commits<CR>', 'git commits' },
 	}
-
 }
 
 return M
