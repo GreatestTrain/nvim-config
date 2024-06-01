7fb1ca121216bc5895af6d6f1eeaa2ccdfeefe65
GreatestTrain@archie-box <96893357+GreatestTrain@users.noreply.github.com>
2024-06-01 10:39:21 -0500

    Sat Jun  1 10:39:21 -05 2024

--- lua/options.lua
+++ lua/options.lua
@@ -1,8 +1,8 @@
 -- Theme options
 vim.o.number	 	= true
+vim.o.list			= true
 vim.o.shiftwidth 	= 4
 vim.o.tabstop	 	= 4
-vim.o.list			= 4
 vim.cmd [[ :colorscheme murphy ]]
 
 -- Load neovide opts if necessary - lua/neovide_options.lua
