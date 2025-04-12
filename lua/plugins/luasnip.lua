c5d86ee9d5ce749755b5fefcd88d7b766957587a
GreatestTrain <96893357+GreatestTrain@users.noreply.github.com>
2025-04-12 18:06:42 -0500

    2025-04-12 18:06:42-05:00

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -2,6 +2,6 @@ local m = {}
 
 m[1] = "L3MON4D3/LuaSnip"
 m.name = "luasnip"
-m.build = "make install_jsregexp"
+m.build = vim.g.os == "Windows" and "make install_jsregexp SHELL=sh.exe .SHELLFLAGS=-c" or "make install_jsregexp"
 
 return m
